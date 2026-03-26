86e3fddcaed82fcd1b9b143405e413d652d5df01
Pablo <pablofguillen1503@gmail.com>
2026-03-26 09:47:54 +0100

    Actualizamos angular a la versión 21

--- photomodel/src/app/app.component.ts
+++ photomodel/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AvatarModule, ButtonModule, ToolbarModule, TopMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'photomodel';
 }
